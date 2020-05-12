b6e15ea57e7a0d603a49ed17721577a84ecd2790
Felix Lopez <felix.lopez@vacasa.com>
2020-05-12 16:27:03 -0700

    Complete array rotation challenge

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -5,7 +5,7 @@
 // e f g
 
 
-const twoDimensialArray = [
+const twoDimensionalArray = [
     [1, 1, 1, 0, 0, 0],
     [0, 2, 0, 0, 0, 0],
     [1, 1, 1, 0, 0, 0],
@@ -14,7 +14,7 @@ const twoDimensialArray = [
     [0, 0, 0, 0, 0, 0]
 ];
 
-function testing(twoD) {
+function hourglass(twoD) {
     // Since we know X and Y limit is 3.
     const maxHourGlassY = 3;
     const maxHourGlassX = 3;
@@ -40,4 +40,19 @@ function testing(twoD) {
     return totalSum;
 }
 
-testing(twoDimensialArray);
+hourglass(twoDimensionalArray);
+
+
+
+// A left rotation operation on an array shifts each of the array's elements 1 unit to the left. 
+// For example, if 2 left rotations are performed on array [1,2,3,4,5], then the array would become [3,4,5,1,2].
+const arr = [1,2,3];
+
+const leftRotation = (a, d) => {
+    const remainder = d % a.length;
+    const portion = a.splice(0, remainder);
+    a.push(...portion);
+    return a;
+}
+
+leftRotation(arr, 2);
